3e8c6920d254fb7ecfb3507db24f9a60c8e4aa60
Deka Louis <83022812+dekalouis@users.noreply.github.com>
2023-08-22 19:56:11 +0700

    Added UI and game functionality through HTML, fixed JS to use gamelogic into html content.

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ console.log("HELLO MA G");
 
 let playerScore = 0;
 let computerScore = 0;
-// first get the computer to send a random choice between rock, paper and scissors
-// 1. make a function that calls a random number between 1-3
-// make the output provide from an array of rock/paper/scis
 
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissor"];
@@ -12,29 +9,30 @@ function getComputerChoice() {
   return computerSelection;
 }
 
-// second make a function that plays a single round
-// takes 2 inputs, player option and computer option
-// make player input insensitive
-// output needs to be returned
-
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "scissor") {
     playerScore++;
+    updateScore();
     return "You Win! Rock beats Scissor!";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     computerScore++;
+    updateScore();
     return "You Lose! Paper beats Rock!";
   } else if (playerSelection === "scissor" && computerSelection === "rock") {
     computerScore++;
+    updateScore();
     return "You Lose! Rock beats Scissor!";
   } else if (playerSelection === "scissor" && computerSelection === "paper") {
     playerScore++;
+    updateScore();
     return "You Win! Scissor beats Paper!";
   } else if (playerSelection === "paper" && computerSelection === "scissor") {
     computerScore++;
+    updateScore();
     return "You Lose! Scissor beats Paper!";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
+    updateScore();
     return "You Win! Paper beats Rock!";
   } else if (playerSelection === computerSelection) {
     return "Draw!";
@@ -43,47 +41,97 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-//finally make a game roudn that does 5 plays
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissor");
 
-function game() {
-  if (playerScore === 0 && computerScore === 0) {
-    let playerSelection = prompt("what is your input?").toLowerCase();
-    let computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    playerSelection = prompt("what is your input?").toLowerCase();
-    computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    playerSelection = prompt("what is your input?").toLowerCase();
-    computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    playerSelection = prompt("what is your input?").toLowerCase();
-    computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-
-    playerSelection = prompt("what is your input?").toLowerCase();
-    computerSelection = getComputerChoice();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    if (playerScore > computerScore && computerScore != playerScore) {
-      console.log("You won the most points! Game over!");
-    } else if (computerScore > playerScore && computerScore != playerScore) {
-      console.log("Computer won the most points! You lose!");
-    } else if (computerScore === playerScore) {
-      console.log("All draw! Try again");
-    }
+function updateScore() {
+  const scoreBoard = document.getElementById("scoreContainer");
+  scoreBoard.innerHTML = `Player: ${playerScore} | Computer: ${computerScore}`;
+  if (playerScore >= 5) {
+    announceWinner("Player");
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorBtn.disabled = true;
+    refreshPageWithDelay(4000);
+  } else if (computerScore >= 5) {
+    announceWinner("Computer");
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorBtn.disabled = true;
+    refreshPageWithDelay(4000);
   }
 }
 
-game();
+function refreshPageWithDelay(delay) {
+  setTimeout(function () {
+    location.reload();
+  }, delay);
+}
+
+function announceWinner(winner) {
+  const winnerContainer = document.getElementById("winnerContainer");
+  winnerContainer.innerHTML = `${winner} wins the game!`;
+}
+
+function handleClick(event) {
+  const logContainer = document.getElementById("logContainer");
+  const playerSelection = event.target.id;
+  const computerSelection = getComputerChoice();
+
+  const round = `You selected ${playerSelection}, Computer selected ${computerSelection}`;
+
+  const result = playRound(playerSelection, computerSelection);
+  console.log(result);
+
+  logContainer.innerHTML = round + "<br>" + result;
+}
+
+rockBtn.addEventListener("click", handleClick);
+paperBtn.addEventListener("click", handleClick);
+scissorBtn.addEventListener("click", handleClick);
+
+//finally make a game roudn that does 5 plays
+
+// function game() {
+//   if (playerScore === 0 && computerScore === 0) {
+//     let playerSelection = prompt("what is your input?").toLowerCase();
+//     let computerSelection = getComputerChoice();
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+
+//     playerSelection = prompt("what is your input?").toLowerCase();
+//     computerSelection = getComputerChoice();
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+
+//     playerSelection = prompt("what is your input?").toLowerCase();
+//     computerSelection = getComputerChoice();
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+
+//     playerSelection = prompt("what is your input?").toLowerCase();
+//     computerSelection = getComputerChoice();
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+
+//     playerSelection = prompt("what is your input?").toLowerCase();
+//     computerSelection = getComputerChoice();
+//     console.log(playerSelection);
+//     console.log(computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
+//     if (playerScore > computerScore && computerScore != playerScore) {
+//       console.log("You won the most points! Game over!");
+//     } else if (computerScore > playerScore && computerScore != playerScore) {
+//       console.log("Computer won the most points! You lose!");
+//     } else if (computerScore === playerScore) {
+//       console.log("All draw! Try again");
+//     }
+//   }
+// }
+
+// game();
